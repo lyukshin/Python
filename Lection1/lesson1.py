bbde69f455dd17c10f4fa0974d51cf906e903743
Oleg Lyukshin <oleg@lyukshin.ru>
2023-04-23 07:26:05 +0300

    Add Lection 1 - Homework v1

---
+++ Lection1/lesson1.py
@@ -0,0 +1,13 @@
+# ввести два числа и определить какое число больше
+
+x = int(input('введите число Х - '))
+y = int(input('введите число Y - '))
+
+if x > y : 
+    print ("x > y")
+elif x == y:
+    print("x == y")
+else:
+    print("x < y")
+    
+print(x==y)
